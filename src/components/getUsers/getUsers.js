650386eae3e865c7c8a687dde7011bf58c48aa48
miguelortis <pedroarguelles1996@gmail.com>
2022-06-19 00:59:27 -0430

    fix

--- src/components/getUsers/getUsers.js
+++ src/components/getUsers/getUsers.js
@@ -12,110 +12,103 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { styled } from '@mui/material/styles';
 import Badge from '@mui/material/Badge';
 const StyledBadge = styled(Badge)(({ theme }) => ({
-    '& .MuiBadge-badge': {
-        backgroundColor: '#44b700',
-        color: '#44b700',
-        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-        '&::after': {
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            borderRadius: '50%',
-            animation: 'ripple 1.2s infinite ease-in-out',
-            border: '1px solid currentColor',
-            content: '""',
-        },
-    },
-    '@keyframes ripple': {
-        '0%': {
-            transform: 'scale(.8)',
-            opacity: 1,
-        },
-        '100%': {
-            transform: 'scale(2.4)',
-            opacity: 0,
-        },
-    },
+	'& .MuiBadge-badge': {
+		backgroundColor: '#44b700',
+		color: '#44b700',
+		boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+		'&::after': {
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			width: '100%',
+			height: '100%',
+			borderRadius: '50%',
+			animation: 'ripple 1.2s infinite ease-in-out',
+			border: '1px solid currentColor',
+			content: '""',
+		},
+	},
+	'@keyframes ripple': {
+		'0%': {
+			transform: 'scale(.8)',
+			opacity: 1,
+		},
+		'100%': {
+			transform: 'scale(2.4)',
+			opacity: 0,
+		},
+	},
 }));
 export function GetUsers() {
-    const {
-        dispatch,
-    } = useContext(Context)
-    const [users, setUsers] = useState([]);
+	const {
+		dispatch,
+	} = useContext(Context)
+	const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        socket.on("getUsers", (users) => {
-            setUsers(users);
-            dispatch({ type: "SET_USERS", payload: users });
-        });
-    }, [dispatch]);
-    const online = {
-        height: '10px',
-        width: '10px',
-        backgroundColor: 'rgba(25, 255, 4, 0.89)',
-        borderRadius: '50px',
-        border: '1px solid rgba(5, 155, 30, 0.89)',
-    }
-    const handleClose = () => {
-        dispatch({ type: "SET_BUTTON_INFO", payload: false })
-    }
-    return (
-        <div >
-            <List dense sx={{
-                width: '100%', maxWidth: { md: 360 }, bgcolor: 'background.paper', minHeight: {
-                    xs: 'calc(100vh - 83px)',
-                    md: 'calc(100vh - 195px)'
-                }
-            }}>
-                <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleClose}
-                    color="inherit"
-                    sx={{ position: 'absolute', top: '0', display: { xs: 'flex', md: 'none' } }}
-                >
-                    <ArrowBackIcon sx={{ fontSize: 40 }} />
-                </IconButton>
-                <Typography variant="h5" gutterBottom component="div" sx={{ pt: 1, pb: 2 }}>
-                    Usuarios Conectados
-                </Typography>
-                <Divider sx={{ margin: '5px 0' }} />
-                {users.length <= 1
-                    ? <ListItem sx={{ textAlign: 'center' }}><ListItemText primary='No se ha conectado ningun usuario' /></ListItem>
-                    : users.map((user, i) => {
-                        if (user.socketId === socket.id) {
-                            return null
-                        } else {
-                            return (
-                                <ListItem
-                                    key={i}
-                                    secondaryAction={
-                                        <div style={online} />
-                                    }
-                                    disablePadding
-                                >
-                                    <ListItemButton>
-                                        <ListItemAvatar>
+	useEffect(() => {
+		socket.on("getUsers", (users) => {
+			setUsers(users);
+			dispatch({ type: "SET_USERS", payload: users });
+		});
+	}, [dispatch]);
 
-                                            <StyledBadge
-                                                overlap="circular"
-                                                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                                                variant="dot"
-                                            >
-                                                <Avatar alt="user.name" src="/static/images/avatar/1.jpg" >{user.name.charAt(0).toUpperCase()}</Avatar>
-                                            </StyledBadge>
-                                        </ListItemAvatar>
-                                        <ListItemText primary={user.name} />
-                                    </ListItemButton>
-                                </ListItem>
-                            );
-                        }
-                    })}
-            </List>
-        </div>
-    );
+	const handleClose = () => {
+		dispatch({ type: "SET_BUTTON_INFO", payload: false })
+	}
+
+
+	return (
+		<div >
+			<List dense sx={{
+				width: '100%', maxWidth: { md: 360 }, bgcolor: 'background.paper', minHeight: {
+					xs: 'calc(100vh - 83px)',
+					md: 'calc(100vh - 195px)'
+				}
+			}}>
+				<IconButton
+					size="large"
+					aria-label="account of current user"
+					aria-controls="menu-appbar"
+					aria-haspopup="true"
+					onClick={handleClose}
+					color="inherit"
+					sx={{ position: 'absolute', top: '0', display: { xs: 'flex', md: 'none' } }}
+				>
+					<ArrowBackIcon sx={{ fontSize: 40 }} />
+				</IconButton>
+				<Typography variant="h5" gutterBottom component="div" sx={{ pt: 1, pb: 2 }}>
+					Usuarios Conectados
+				</Typography>
+				<Divider sx={{ margin: '5px 0' }} />
+				{users.length <= 1
+					? <ListItem sx={{ textAlign: 'center' }}><ListItemText primary='No se ha conectado ningun usuario' /></ListItem>
+					: users.map((user, i) => {
+						if (user.socketId === socket.id) {
+							return null
+						} else {
+							return (
+								<ListItem
+									key={i}
+									disablePadding
+								>
+									<ListItemButton>
+										<ListItemAvatar>
+
+											<StyledBadge
+												overlap="circular"
+												anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+												variant="dot"
+											>
+												<Avatar alt="user.name" src="/static/images/avatar/1.jpg" >{user.name.charAt(0).toUpperCase()}</Avatar>
+											</StyledBadge>
+										</ListItemAvatar>
+										<ListItemText primary={user.name} />
+									</ListItemButton>
+								</ListItem>
+							);
+						}
+					})}
+			</List>
+		</div>
+	);
 }
